8c49761fe54adbdf331fc184d383ac697c036440
Elias Gabriel <egabriel@olin.edu>
2019-10-24 00:15:44 -0400

    Add controls code

---
+++ run/controls.cpp
@@ -0,0 +1,46 @@
+/* 
+ * This source file contains all the logic responsible for moving the robot given a
+ * specific heading and magnitude.
+ * 
+ * @authors: Elias
+ */
+#include "controls.h"
+
+const uint8_t RANGE_OF_MOTION = 90;
+const uint8_t NUM_SERVOS = 4;
+const uint8_t SERVO_PINS[] = {0, 1, 2, 3};
+const Servo SERVOS[4];
+const uint8_t RANGE_OF_MOTION;
+
+double leftStrideCoefficient;
+double rightStrideCoefficient;
+int currentStrideAngle = 0;
+boolean stepping = true;
+
+void attachServos() {
+  for(int i = 0; i < NUM_SERVOS; i++) {
+    SERVOS[i].attach(SERVO_PINS[i]);
+    SERVOS[i].write(0);
+  }
+}
+
+void calculateStrideCoefficients(uint16_t heading, uint8_t magnitude) {
+  if(heading > 180) {
+    heading -= 180;
+    magnitude *= -1;
+  }
+
+  leftStrideCoefficient = heading / 180.0;
+  rightStrideCoefficient = 1 - leftStride;
+}
+
+void moveAtHeading() {
+  SERVOS[0].write(floor(leftStrideCoefficient * i));
+  SERVOS[1].write(floor(rightStrideCoefficient * (RANGE_OF_MOTION - i)));
+  SERVOS[2].write(i);
+  SERVOS[3].write(RANGE_OF_MOTION - i);
+  
+  currentStrideAngle = currentStrideAngle + (stepping ? 1 : -1);
+  if(currentStrideAngle == RANGE_OF_MOTION || currentStrideAngle == 0) stepping = !stepping;
+  delay(15);
+}
--- run/controls.h
+++ run/controls.h
@@ -1,13 +1,17 @@
-/* controls.h
- *
- * FollowBot POE Project
+/* 
+ * This header file contains all the constants, buildtime, and runtime variables necessary
+ * to compile and execute the robot controls.
  * 
  * @authors: Elias
  */
-
-
 #ifndef controls_h
 #define controls_h
 
+#include <stdint.h>
+#include <Servo.h>
+
+void attachServos();
+void calculateStrideCoefficients(uint16_t heading, uint8_t magnitude);
+void moveAtHeading();
 
 #endif
