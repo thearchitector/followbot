48b078f6c5baaad688ea815482a8e47fd8015b6c
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2019-10-25 12:21:30 -0400

    Add route to about the team page

--- website/followbot.py
+++ website/followbot.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/about-team')
+def about():
+    return render_template('aboutus.html')
+
 if __name__ == '__main__':
     app.run()
