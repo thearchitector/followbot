6be34013fa8218d077d7485eab1719aadeea6cb3
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2019-10-25 11:24:34 -0400

    Fix syntax error

--- website/followbot.py
+++ website/followbot.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def homepage:
+def homepage():
     return render_template('homepage.html')
-
-@app.route
 
 if __name__ == '__main__':
     app.run()
