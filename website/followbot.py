9d3631f0cddb25acbf5281f6ef8807b4be135882
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2019-10-26 16:45:46 -0400

    Add new pages to webiste

--- website/followbot.py
+++ website/followbot.py
@@ -6,25 +6,45 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/about-team')
-def about():
-    return render_template('aboutus.html')
-
-@app.route('/mechanical')
-def mechanical():
-    return render_template('mechanical.html')
-
-@app.route('/electrical')
-def electrical():
-    return render_template('electrical.html')
-
-@app.route('/software')
-def software():
-    return render_template('software.html')
-
 @app.route('/process')
 def process():
     return render_template('process.html')
+
+@app.route('process/sprint-1')
+def sprint1():
+    return render_template('sprint1.html')
+
+@app.route('process/sprint-2')
+def sprint1():
+    return render_template('sprint2.html')
+
+@app.route('process/sprint-3')
+def sprint1():
+    return render_template('sprint3.html')
+
+@app.route('/systems')
+def systems():
+    return render_template('systems.html')
+
+@app.route('systems/mechanical')
+def mechanical():
+    return render_template('mechanical.html')
+
+@app.route('systems/electrical')
+def electrical():
+    return render_template('electrical.html')
+
+@app.route('systems/software')
+def software():
+    return render_template('software.html')
+
+@app.route('/budget')
+def budget():
+    return render_template('budget.html')
+
+@app.route('/about-team')
+def about():
+    return render_template('aboutus.html')
 
 if __name__ == '__main__':
     app.run()
