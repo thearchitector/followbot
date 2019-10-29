c2574d1f0b7e187ba09f36133c215337530915c0
Duncan Mazza <dmazza@olin.edu>
2019-10-29 10:28:17 -0400

    Add sensing skeleton code

---
+++ sense/README.md
@@ -0,0 +1,2 @@
+# Running
+- Make sure that you have installed OpenCV and added it to your enviornment variables
---
+++ sense/cmake-build-debug/CMakeFiles/sense.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sense.dir/main.cpp.obj"
+  "sense.exe"
+  "sense.exe.manifest"
+  "sense.lib"
+  "sense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
