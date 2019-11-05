6f30ec1de07043219f30d1fbfeb88e88a61a7083
Elias Gabriel <me@eliasfgabriel.com>
2019-11-04 21:50:01 -0500

    Update dockin.sh

--- raspi/dockin.sh
+++ raspi/dockin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-xhost +local:
-docker run -ite DISPLAY=$DISPLAY -e QT_GRAPHICSSYSTEM=native --net host thearchitector/followbot:dev
+if [ -z $(docker ps -a --format "{{.Names}}" | grep "followbot") ]; then
+    xhost +local:
+    docker run -ite DISPLAY=$DISPLAY -e QT_GRAPHICSSYSTEM=native --net=host -v "/home/developer/followbot" -v "/dev/:/dev/:rw" -v "/media/$USER/:/media/$USER/:rw" --name=followbot thearchitector/followbot:dev
+else
+    docker exec -it followbot bash
+fi
