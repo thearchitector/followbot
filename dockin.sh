189834ac03f44a8e304d441e4228c1f9ccd8b060
Elias Gabriel <egabriel@olin.edu>
2019-11-08 10:14:30 -0500

    Move dockin

--- dockin.sh
+++ dockin.sh
@@ -4,5 +4,5 @@ if [ -z $(docker ps -a --format "{{.Names}}" | grep "followbot") ]; then
     xhost +local:
     docker run -ite DISPLAY=$DISPLAY -e QT_GRAPHICSSYSTEM=native --net=host -v "/home/developer/followbot" -v "/dev/:/dev/:rw" -v "/media/$USER/:/media/$USER/:rw" --name=followbot thearchitector/followbot:dev
 else
-    docker exec -it followbot bash
+    docker start -ai followbot
 fi
