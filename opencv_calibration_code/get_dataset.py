c842d3e352aee8ec888c83b435b7191b792ceef9
Duncan Mazza <dmazza@olin.edu>
2019-11-19 10:12:40 -0500

    Make data collection script more robust and easy to use

--- opencv_calibration_code/get_dataset.py
+++ opencv_calibration_code/get_dataset.py
@@ -1,19 +1,52 @@
 import cv2
 import time
 import os
+import glob
+
+CALIBRATE_IMGS_FOLDER = "test_folder"
+
+# NOTE: double check these values every time the cameras are plugged back in
+LEFT_CAMERA_IDX = 2
+RIGHT_CAMERA_IDX = 1
+
 
 def collect_images():
-    output_path = os.path.join(os.getcwd(), "test_images")
-
+    output_path = os.path.join(os.getcwd(), CALIBRATE_IMGS_FOLDER)
+    print("Running from directory '{}/'; will store calibration images in '{}/'".format(os.getcwd(),
+                                                                                        CALIBRATE_IMGS_FOLDER))
     if os.path.exists(output_path):
-        os.system("rm -rf " + output_path)
-    os.makedirs(output_path, exist_ok=False)
+        if len(os.listdir(output_path)) != 0:
+            print(
+                "To proceed with collecting the dataset and writing images to '{}/', the contents of the '{}/' directory "
+                "need to be deleted. Delete all files in `{}`?".format(output_path, CALIBRATE_IMGS_FOLDER, output_path))
+            while True:
+                response = input("y/n -> ")
+                response = response.lower()
+                if response == "y" or response == "yes":
+                    files = glob.glob(output_path + "/*")
+                    for f in files:
+                        print("  deleting '{}'".format(f))
+                        os.remove(f)
+                    break
+                elif response == "n" or response == "no":
+                    print("Aborting data collection")
+                    exit(-1)
+                else:
+                    print("Invalid response '{}', please try again".format(response))
+                    continue
+    else:
+        print("Creating directory '{}'".format(output_path))
+        os.makedirs(output_path)
 
-    xml_file = open(os.path.join(output_path, "test_images.xml"), 'w')
+    capL = cv2.VideoCapture(LEFT_CAMERA_IDX)
+    capR = cv2.VideoCapture(RIGHT_CAMERA_IDX)
+
+    if not capL.isOpened() or not capR.isOpened():
+        print("Aborting because one or both of the cameras couldn't be opened")
+        exit(-1)
+
+    xml_file = open(os.path.join(output_path, "calibrate_imgs.xml"), 'w')
     xml_text = ['<?xml version="1.0"?><opencv_storage><imagelist>', '', '</imagelist></opencv_storage>']
-
-    capL = cv2.VideoCapture(2)
-    capR = cv2.VideoCapture(1)
 
     # uncomment if 720x1280 resolution is desired
     # capL.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -54,12 +87,13 @@ def collect_images():
 
         cv2.imwrite(os.path.join(output_path, filenameL), imgL)
         cv2.imwrite(os.path.join(output_path, filenameR), imgR)
-        xml_text[1] += os.path.join('"test_images', filenameL) + '" ' + os.path.join('"test_images', filenameR + '" ')
-
+        xml_text[1] += os.path.join('"calibrate_imgs', filenameL) + '" ' + os.path.join('"calibrate_imgs',
+                                                                                        filenameR + '" ')
         print("Captured ", i)
 
     xml_file.write("".join(xml_text))
     xml_file.close()
+
 
 if __name__ == "__main__":
     collect_images()
