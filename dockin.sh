39f20c3adac69515e6a1961ba35351922946a3fc
Elias Gabriel <egabriel@olin.edu>
2019-11-22 10:48:41 -0500

    Fix dockin

--- dockin.sh
+++ dockin.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 if [ -z $(docker ps -a --format "{{.Names}}" | grep "followbot") ]; then
-    xhost +local:
-    docker run -ite DISPLAY=$DISPLAY --rm --network=host --privileged -v "/home/developer/followbot" -v "/dev/:/dev/:rw" -v "/media/$USER/:/media/$USER/:rw" --name=followbot thearchitector/followbot:dev
+    #xhost +local:
+    docker run -it \
+    	--network=host \
+	--privileged \
+	-w "/home/developer/followbot" \
+	-e DISPLAY=$DISPLAY \
+	-e HOME="/home/developer" \
+	-v "/tmp/.X11-unix:/tmp/.X11-unix" \
+	-v "/dev/:/dev/:rw" \
+	-v "/media/$USER/:/media/$USER/:rw" \
+	-v "followbot-volume:/home/developer/followbot" \
+	--name=followbot thearchitector/followbot:dev 
 else
     docker start -ai followbot
 fi
