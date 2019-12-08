374b7a8feabe7d51f6645b4b345674493994e03c
duncanmazza <dmazza@olin.edu>
2019-12-08 04:20:37 +0000

    Add beginnings of astar separation

--- raspi/followbot_ws/src/followbot/include/cloud.hpp
+++ raspi/followbot_ws/src/followbot/include/cloud.hpp
@@ -102,22 +102,12 @@ class PointCloud {
     std::vector<cv::Point2f> buffer;
     std::map<IntPair, bool> occupied;  // map of the occupancy grid
 
-    bool isUnBlocked(const IntPair &point);
-    static bool isDestination(const IntPair &point, const IntPair &dest);
-    static float calculateH(const IntPair &point, const IntPair &dest);
-    static std::vector<AStarNode> makePath(std::map<IntPair, AStarNode> &allMap, const IntPair &dest);
-    void fillOccupanyGrid();
-
     public:
         void setupStereoCameras();
         void collectPointCloud(cv::Mat &imgL_remap_3channel, cv::Mat &pointcloud);
         void releaseCameras();
 
-        #ifndef PRODUCTION
-        void showPersonLoc(const followbot::Point2 &personLoc);
-        #endif
 
-        std::vector<AStarNode> findAStarPath(const IntPair &dest);
 };
 
 #endif //FOLLOWBOT_CLOUD_HPP
