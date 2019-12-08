3126d87380735433011a8ad842a1b3856a640057
Elias Gabriel <egabriel@olin.edu>
2019-12-08 04:24:58 +0000

    Add Buffer message

--- raspi/followbot_ws/src/followbot/include/loc_and_pc.hpp
+++ raspi/followbot_ws/src/followbot/include/loc_and_pc.hpp
@@ -4,8 +4,13 @@
 #include <ros/ros.h>
 #include <cloud.hpp>
 #include <human.hpp>
-#include <chrono>
 #include <followbot/Point2.h>
+#include <followbot/Buffer.h>
+#include <std_msgs/UInt16.h>
+
+#ifdef PRODUCTION
+#include <chrono>
+#endif
 
 
 #endif //FOLLOWBOT_LOC_AND_PC_HPP
--- raspi/followbot_ws/src/followbot/src/loc_and_pc.cpp
+++ raspi/followbot_ws/src/followbot/src/loc_and_pc.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "seeker");
 
     #ifdef PRODUCTION
-    std::cout << "-- IN PRODUCTION MODE -- (will suppress data visualizations) " << std::endl;
+    std::cout << "-- IN PRODUCTION MODE (will suppress data visualizations) -- " << std::endl;
     #else
     std::cout << "-- NOT IN PRODUCTION MODE (will show data visualizations) --" << std::endl;
     #endif
@@ -13,35 +13,43 @@ int main(int argc, char **argv) {
     PointCloud pc{};
     HumanDetector hd{};
 
+    ros::Publisher point_cloud = n.advertise<followbot::Buffer>("point_cloud", 1);
     ros::Publisher human_pose = n.advertise<followbot::Point2>("human_pose", 1);
-    ros::Rate loop_rate(30);
+    ros::Publisher desired_heading = n.advertise<std_msgs::UInt16>("desired_heading", 1);
+    ros::Rate loop_rate(5);
 
     pc.setupStereoCameras();
     hd.setupNetwork();
 
-    cv::Mat rectifiedImg;
+    followbot::Buffer buffer_msg;
     followbot::Point2 pose_msg;
+    std_msgs::UInt16 heading_msg;
+
+    cv::Mat rectifiedImg;
     cv::Mat xyz;
+
     while (ros::ok()) {
-//        std::clock_t begin = clock();
+        #ifndef PRODUCTION
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        #endif
 
         pc.collectPointCloud(rectifiedImg, xyz);
         hd.getHumanPosition(rectifiedImg, xyz, pose_msg);
 
-        std::cout << "Person loc: " << pose_msg.x << ", " << pose_msg.z << std::endl;
+        buffer_msg.buffer.clear();
+        heading_msg.data = (short)std::round(std::atan2(pose_msg.z, pose_msg.x));
+        std::cout << "Person: " << pose_msg.x << ", " << pose_msg.z << std::endl;
 
         #ifndef PRODUCTION
         pc.showPersonLoc(pose_msg);
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Hz: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
         #endif
 
+        point_cloud.publish(buffer_msg);
         human_pose.publish(pose_msg);
+        desired_heading.publish(heading_msg);
         ros::spinOnce();
-
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        std::cout << "loop time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() <<
-            "ms" << std::endl;
-
         loop_rate.sleep();
     }
 
