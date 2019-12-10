d32d4676eb40f8f66f71ba70b675371f315a2056
Elias Gabriel <egabriel@olin.edu>
2019-12-09 19:52:41 -0500

    Change dockin settings

--- dockin.sh
+++ dockin.sh
@@ -12,18 +12,15 @@ if [ -z $(docker ps -a --format "{{.Names}}" | grep "followbot") ]; then
 	    -v "followbot-volume:/home/bot/followbot" \
 	    --name=followbot thearchitector/followbot:raspi
     else
-        xhost +local:
         docker run -it \
             --network=host \
   	    --privileged \
 	    -e DISPLAY=$DISPLAY \
-	    #-v "/tmp/.X11-unix:/tmp/.X11-unix" \
-	    -v "/dev/:/dev/:rw" \
-	    -v "/media/$USER/:/media/$USER/:rw" \
+	    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+	    -v "/dev:/dev" \
 	    -v "followbot-volume:/home/developer/followbot" \
-	    --name=followbot thearchitector/followbot:dev
+	    --name=followbot thearchitector/followbot:dev bash
     fi
 else
-    xhost +local:
     docker start -ai followbot
 fi
